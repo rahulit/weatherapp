63a765479e6a4b37357e95cb650a9860a79481db
rahul <rahul.khandalkar@gmail.com>
2021-05-31 00:08:30 -0700

    Backend WeatherApp - Initial Setup

---
+++ src/main/java/backendweatherapp/com/example/backendweatherapp/BackendWeatherappApplication.java
@@ -0,0 +1,13 @@
+package backendweatherapp.com.example.backendweatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWeatherappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWeatherappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backendweatherapp/com/example/backendweatherapp/BackendWeatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package backendweatherapp.com.example.backendweatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWeatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
